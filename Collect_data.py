d1541fd8a8ce46479708bf802f5121a63119e681
dnjonesx <dnjonesx@icloud.com>
2026-04-30 10:04:44 -0400

    I added some new files
    I added Collect_data.py in order to actually start the process of processing some data from an API. I added an import from said API although I haven't quite gotten it figured out yet. I am trying to have the pip install work within a venv which required the requirements.txt file and .gitignore. I updated README with instructions on using said venv but they are actually not very accurate because I still have to figure out some hiccups with it.

---
+++ Collect_data.py
@@ -0,0 +1,4 @@
+#I am thinking of using Open Mateo's free API for the weather. They also have a historical API which includes weather data from 1940-now. Violet might find this useful for his data processing.
+
+#this will need to be installed prior to running this program via pip. See instructions in the README.md file
+import openmateo_requests

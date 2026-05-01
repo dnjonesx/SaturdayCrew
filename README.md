be588a79dbf5e0ccf854dbffadb580bfdad091ce
dnjonesx <dnjonesx@icloud.com>
2026-05-01 08:28:05 -0400

    Changed a lot of things that related to the venv for installing dependencies
    I was being dumb before with how I was doing it with a venv that was a pain so I figured out a way where the program py file will actually check if you have the needed dependencies and install them if you don't. This is way easier as a whole and no longer requires the requirements.txt file or the .gitignore file. I also modified the README so that it no longer includes instructions for how to interact with the venv.

--- README.md
+++ README.md
@@ -2,7 +2,3 @@
 This is a project that Emily, Violet and I will work on.
 
 We are thinking that we will use Python to track a free weather API that collects data and puts it into an SQL database and is showcased by a Javascript frontend. 
-
-In order to operate with the API, you will need to create a separate venv within this folder by running python -m venv venv source venv/bin/activate on linux/mac or python -m venv venv source venv\Scripts\activate. Then you will need to run pip install -r requirements.txt.
-
-There will be a .gitignore file that will include the venv so that it is not shared via git.
--- backend/Collect_data.py
+++ backend/Collect_data.py
@@ -1,4 +1,22 @@
-#I am thinking of using Open Mateo's free API for the weather. They also have a historical API which includes weather data from 1940-now. Violet might find this useful for his data processing.
+#I am thinking of using Open Meteo's free API for the weather. They also have a historical API which includes weather data from 1940-now. Violet might find this useful for his data processing.
 
-#this will need to be installed prior to running this program via pip. See instructions in the README.md file
-import openmateo_requests
+#First I want to have the program itself run the necessary commands to download the imported packages.
+import subprocess
+import sys
+
+def install_dependencies():
+    packages = ["openmeteo-requests", "requests-cache", "retry-requests"]
+    for package in packages:
+        try:
+            __import__(package.replace('-', '_'))
+        except ImportError:
+            print(f"Importing {package}...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install_dependencies()
+
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+print("all dependencies are ready")

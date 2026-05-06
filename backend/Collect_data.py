8af224b8039e98f159209e4cd60ee4dd04176c69
dnjonesx <dnjonesx@icloud.com>
2026-05-06 16:57:08 -0400

    I added more function
    This edit allows for it to show precipitation and wind speed with words rather than numbers.

--- backend/Collect_data.py
+++ backend/Collect_data.py
@@ -21,15 +21,15 @@ def generate_longitude():
     return round(ran_long, 5)
 ran_long = generate_longitude()
 
-print(f"Latitude: ", {ran_lat}, "Longitude: ", {ran_long})
+print(f"Latitude:", {ran_lat})
+print(f"Longitude:", {ran_long})
 
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
     "latitude": ran_lat,
     "longitude": ran_long,
-    "current": "temperature_2m",
-    "forecast_days": 1,
-    "temperature_unit": "fahrenheit",
+    "current": ["temperature_2m", "precipitation", "wind_speed_10m"],
+    "temperature_unit": "celsius",
 }
 
 responses = openmeteo.weather_api(url, params = params)
@@ -37,5 +37,48 @@ response = responses[0]
 
 current = response.Current()
 current_temperature_2m = round(current.Variables(0).Value(), 2)
+current_precipitation = current.Variables(1).Value()
+current_wind_speed_10m = current.Variables(2).Value()
 
-print(f"Current Temperature: {current_temperature_2m}")
+wind = round(current_wind_speed_10m, None)
+rain = round(current_precipitation, None)
+
+def convert_kmh_to_word(wind):
+    if wind == 0:
+        air = "None"
+    elif wind in range(0, 19):
+        air = "Light"
+    elif wind in range(20, 38):
+        air = "Breezy"
+    elif wind in range(39, 49):
+        air = "Strong Breeze"
+    elif wind in range(50, 61):
+        air = "Very Windy"
+    elif wind in range(62, 88):
+        air = "Gale"
+    elif wind in range(89, 117):
+        air = "Bad Storm"
+    else:
+        air = "Hurricane"
+    return air
+
+air = convert_kmh_to_word(wind)
+
+def convert_mm_to_word(rain):
+    if rain == 0:
+        water = "None"
+    elif rain in range(0, 3):
+        water = "Light"
+    elif rain in range(3, 8):
+        water = "Moderate"
+    elif rain in range(8, 50):
+        water = "Heavy"
+    else:
+        water = "Violent"
+    return water
+
+water = convert_mm_to_word(rain)
+
+print(f"Current Temperature: {current_temperature_2m} °C")
+print(f"Current Precipitation: {water}")
+print(f"Wind Speed: {air}")
